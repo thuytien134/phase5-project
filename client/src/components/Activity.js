7aa5fb01b136bb79dc6b590c8527659521f9bd82
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-09-16 16:18:37 -0700

    updated

--- client/src/components/Activity.js
+++ client/src/components/Activity.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import Logout from './Logout'
+import { Tabs, Tab } from '@mui/material'
 
-export default function Activity({onLogout,setIsLogin}) {
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import ReviewsIcon from '@mui/icons-material/Reviews';
+
+export default function Activity({ onLogout, setIsLogin }) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div>
+      <Tabs
+        value={value} onChange={handleChange} aria-label="icon tabs example"
+        >
+        <Tab icon={"🛍️"} label="bag" />
+        <Tab icon={"reviews"} aria-label="reviews" />
+      </Tabs>
       <Logout onLogout={onLogout} setIsLogin={setIsLogin}/>
     </div>
   )
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+
 import { Alert } from 'react-bootstrap';
 
 
